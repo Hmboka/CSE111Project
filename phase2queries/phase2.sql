bc5bdfc712a8a3a215f72fdd664951ab5251d918
Hmboka <habibatumboka@gmail.com>
2020-11-12 01:42:32 -0800

    edited some comments

--- phase2queries/phase2.sql
+++ phase2queries/phase2.sql
@@ -27,20 +27,20 @@ from books;
 
 
 --------------   4   --------------
---insert into fav author #WORKS
+--insert into fav author 
 INSERT INTO faveauthors (fa_name, fa_myrating)
 VALUES ('HabiTesting', 2.4);
 
 
 --------------   5   --------------
---insert into hasread from books table #WORSK (forgot rating input, also best to use bookID instead of title, left title for demonstration purposes)
+--insert into hasread from books table  (forgot rating input, also best to use bookID instead of title, left title for demonstration purposes)
 INSERT INTO hasread (hr_bookID, hr_name, hr_myrating)
 SELECT b_bookID, b_title, 4.2 
 FROM books
 WHERE b_bookID = 22;
 
     --------------   6   --------------
-    --sample to show the next query #WORKS
+    --sample to show the next query 
                     INSERT INTO hasread (hr_bookID, hr_name, hr_myrating)
                     SELECT b_bookID, b_title, 4.20
                     FROM books
@@ -48,33 +48,33 @@ WHERE b_bookID = 22;
                           and b_bookID = 1;
 
 --------------   7   --------------
---Delete books from hasread #WORKS
+--Delete books from hasread 
 DELETE FROM hasread
 WHERE hr_name = 'Harry Potter and the Half-Blood Prince (Harry Potter  #6)'
       and hr_bookID = 1;
 
 
 --------------   8   --------------
---retroactively add your own rating to books added to hasread #WORKS
+--retroactively add your own rating to books added to hasread 
 UPDATE hasread
 SET hr_myrating = 4.2
 WHERE hr_bookID = 77;
 
 
 --------------   9   --------------
---(TRY NOT TO RUN QUERY BC ONCE A BOOK IS DELETED WE MAY NOT GET IT BACK) delete books fom the main book able #WORKS
+--(TRY NOT TO RUN QUERY BC ONCE A BOOK IS DELETED WE MAY NOT GET IT BACK) delete books fom the main book able 
 DELETE FROM books
 WHERE b_bookID = 1;
 
 
 --------------   10   --------------
---delete books based on the author WORKS
+--delete books based on the author 
 DELETE FROM books
 where ad_name = 'testingtesting123'
 
 
 --------------   11   --------------
---find books and author written by blank author that start with The #WORKS
+--find books and author written by blank author that start with "The" 
 SELECT b_title, ad_name
 FROM books, authored
 WHERE b_title like 'The%' AND
@@ -82,7 +82,7 @@ WHERE b_title like 'The%' AND
 
 
 --------------   12   --------------
---find favorite books which are written by a specific author #WORKS
+--find favorite books which are written by a specific author 
 SELECT fb_title
 FROM favebooks
 WHERE fb_bookID IN (SELECT ad_bookID
@@ -103,7 +103,7 @@ WHERE fa_name = 'Random Author';
 
 
 --------------   15   --------------
---find books that were published after 2002 and are on your has read list (table) #WORKS I THINK
+--find books that were published after 2002 and are on your has read list (table)
 SELECT hr_name 
 FROM hasread
 WHERE hr_bookID IN(SELECT p_bookID
@@ -119,7 +119,7 @@ WHERE fa_name = 'Harry Potter and the Half-Blood Prince (Harry Potter  #6)';
 
 
 --------------   17   --------------
---what book has a rating of at least 4.0, written in english, and has at least 600 pages #WORKS
+--what book has a rating of at least 4.0, written in english, and has at least 600 pages
 SELECT b_title, b_numpages, b_langcode
 FROM books
 WHERE b_numpages >=600.0 AND 
@@ -130,7 +130,7 @@ b_bookID IN(SELECT r_bookID
 
 
 --------------   18   --------------
---find faveauthor(s) with highest rating #WORKS
+--find faveauthor(s) with highest rating
 select avg(fa_myrating) as avgRating, fa_name as topAuthor
 from faveauthors
 where fa_myrating =  (select max(famr) from (select avg(fa_myrating) as famr
